63d2eb3cc45d2db5acea6fe83e52d2ec3737b04f
Justin Shreve <justin.shreve@gmail.com>
2013-10-12 14:04:32 -0400

    Sidebar Support. Key Binding Fixes
    I did the key bindings in the wrong format, plus ctrl+alt+f is already
    reserved.
    
    This commit also added sidebar support so you can upload a single file
    by right clicking in sidebar, or multiple files by selecting a
    directory.

--- Cloudup.py
+++ Cloudup.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin, tempfile, base64, os, sys, mimetypes, json, ntpath
+import sublime, sublime_plugin, tempfile, base64, os, sys, mimetypes, json, ntpath, time
 
 sys.path.append( os.path.join( os.path.dirname( __file__ ), "requests" ) )
 import requests
@@ -26,24 +26,53 @@ class CloudupFileCommand( sublime_plugin.TextCommand ):
 		currentfile = self.view.file_name()
 		Cloudupload( currentfile, 'file' )
 
+# @todo This needs to move to a thread
+class CloudupSidebar( sublime_plugin.WindowCommand ):
+	def run(self, paths = [] ):
+		for item in paths:
+			if os.path.isdir( item ):
+				title = str( ntpath.basename( item ) )
+				auth = HTTPBasicAuth( Cloudupload.auth_username(), Cloudupload.auth_password() )
+				s = requests.post( 'https://api.cloudup.com/1/streams', data = { 'title': title }, auth = auth )
+				if s.status_code != 201:
+					sublime.error_message( 'Cloudup connection failed. Please check your username/password and try again' )
+					return
+				stream = s.json()
+
+				for root, subFolders, files in os.walk( item ):
+					for file in files:
+						if file != '.DS_Store':
+							Cloudupload( os.path.join( root, file ), 'file', str( stream['id'] ) )
+
+				url = 'https://cloudup.com/' + stream['id']
+				sublime.set_clipboard( url )
+				sublime.status_message( 'File(s) uploaded. ' + url + ' copied to clipboard' )
+			else:
+				Cloudupload( item, 'file' )
+
 class Cloudupload:
-	def __init__( self, thefile, type ):
-		auth = HTTPBasicAuth( self.auth_username(), self.auth_password() )
+	def __init__( self, thefile, type, stream_id = '' ):
+		auth = HTTPBasicAuth( Cloudupload.auth_username(), Cloudupload.auth_password() )
 
 		if type == 'file':
 			title = str( ntpath.basename( thefile ) )
 		else:
 			title = 'Untitled Snippet'
 
-		# Create a new stream for this snippet
-		s = requests.post( 'https://api.cloudup.com/1/streams', data = { 'title': title }, auth = auth )
-		if s.status_code != 201:
-			sublime.error_message( 'Cloudup connection failed. Please check your username/password and try again' )
-			return
-		stream = s.json()
+		# Create a new stream for this snippet if we didn't already pass it one..
+		if stream_id == '':
+			s = requests.post( 'https://api.cloudup.com/1/streams', data = { 'title': title }, auth = auth )
+			if s.status_code != 201:
+				sublime.error_message( 'Cloudup connection failed. Please check your username/password and try again' )
+				return
+			stream = s.json()
+			stream_id = stream['id']
+			multi = False
+		else:
+			multi = True
 
 		# Create a new item inside the stream with some starter information
-		file_data = { 'filename': thefile, 'mime': 'text/plain', 'stream_id': stream['id'] }
+		file_data = { 'filename': thefile, 'stream_id': stream_id }
 		i = requests.post( 'https://api.cloudup.com/1/items', data = file_data, auth = auth )
 		if i.status_code != 201:
 			sublime.error_message( 'Cloudup connection failed. Please check your username/password and try again' )
@@ -75,20 +104,21 @@ class Cloudupload:
 		headers = { 'Content-Type': 'application/json' }
 		complete = requests.patch( url = 'https://api.cloudup.com/1/items/' + item['id'], data = body, headers = headers, auth = auth )
 		if complete.status_code != 200:
-			sublime.error_message( 'Snippet creation failed. Please check your username/password and try again' )
+			sublime.error_message( 'Cloudup connection failed. Please check your username/password and try again' )
 			return
-		
-		sublime.set_clipboard( item['url'] )
-		sublime.status_message( item['url'] + ' copied to clipboard' )
 
-	def auth_username( self ):
+		if multi == False:
+			sublime.set_clipboard( item['url'] )
+			sublime.status_message( 'Selection uploaded. ' + item['url'] + ' copied to clipboard' )
+
+	def auth_username():
 		settings = sublime.load_settings( 'Cloudup.sublime-settings' )
 		username = settings.get( 'username' )
 		if not username:
 			sublime.error_message( "Please provide your username in the Cloudup.sublime-settings file." )
 		return username
 
-	def auth_password( self ):
+	def auth_password():
 		settings = sublime.load_settings( 'Cloudup.sublime-settings' )
 		password = settings.get( 'password' )
 		if not password:
